157c471cf42cd321f921a169f424dae2a1abc5e3
EchauriMu <edechaurimu@ittepic.edu.mx>
2025-11-07 19:21:54 -0700

    CAMBIO IMPORTANTE PARA JALAR EN FIORI HAGAN PULL

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ module.exports = async (o) =>{
         app.use(express.json({limit: "50mb"}));
 
       app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:8081'],
   credentials: true
 }));
 
